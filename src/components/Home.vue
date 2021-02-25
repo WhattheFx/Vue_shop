d21c3e1a08621e6f332cf36ba7d0d31e22cbe308
WhattheFx <2983313676@qq.com>
2021-02-25 12:12:19 +0800

    渲染菜单

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <!-- 头部区域 -->
     <el-header>
       <div>
-        <img src="../assets/heima.png" alt="">
+        <img src="../assets/heima.png" alt="" />
         <span>电商后台管理系统</span>
       </div>
       <el-button type="info" @click="logout">退出</el-button>
@@ -11,7 +11,32 @@
     <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409Bff">
+          <!-- 一级菜单 -->
+          <!-- index相同，所以你点一个，所有的都展开了 ,所以要加动态数据绑定，但item只接受字符串，但item.id是一个数值，所以要转字符串-->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key='item.id'>
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <el-menu-item :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <!-- 右侧内容主题 -->
       <el-main>Main</el-main>
     </el-container>
@@ -20,7 +45,20 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      //左侧菜单数据
+      menulist: [],
+      iconsObj: {
+        125:'iconfont icon-user',
+        103:'iconfont icon-tijikongjian',
+        101:'iconfont icon-shangpin',
+        102:'iconfont icon-danju',
+        145:'iconfont icon-baobiao'
+      }
+    };
+  },
+  created() {
+    this.getMenuList()
   },
   methods: {
     logout() {
@@ -30,6 +68,13 @@ export default {
       //跳转到登录页
       this.$router.push("/login");
     },
+    //获取所有的菜单
+    async getMenuList() {
+      const{data:res} = await this.$http.get('menus')
+      if(res.meta.status !==200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    }
   },
 };
 </script>
@@ -40,12 +85,12 @@ export default {
 .el-header {
   background-color: #373d41;
   display: flex;
-  justify-content:space-between;
+  justify-content: space-between;
   padding-left: 0;
   align-items: center;
   color: #fff;
   font-size: 20px;
-  >div {
+  > div {
     display: flex;
     align-items: center;
     span {
--- src/main.js
+++ src/main.js
@@ -10,7 +10,14 @@ import './assets/css/global.css'
 //导入axios
 import axios from 'axios'
 
+
 axios.defaults.baseURL = 'http://vueshop.pixiv.download/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  //在最后必须return config
+  return config
+})
 //把这个包挂载到Vue的原型对象上,每一个组件都可以通过this，访问到$http从而发起axios请求，直接this.$http
 Vue.prototype.$http = axios
 
