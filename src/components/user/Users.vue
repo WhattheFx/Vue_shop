980358cb2deb73091057ca3877db9502992fbfba
WhattheFx <2983313676@qq.com>
2021-03-01 17:35:55 +0800

    做了一次修改

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -13,55 +13,118 @@
       <!-- 分栏间隔 -->
       <el-row :gutter="20">
         <el-col :span="7">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-            <el-button type="primary">添加用户</el-button>
+          <el-button type="primary">添加用户</el-button>
         </el-col>
-
       </el-row>
       <!-- 用户列表区域 -->
+
       <el-table :data="userlist" border stripe>
-          <el-table-column label="姓名" prop="username"></el-table-column>
-          <el-table-column label="邮箱" prop="email"></el-table-column>
-          <el-table-column label="电话" prop="mobile"></el-table-column>
-          <el-table-column label="角色" prop="role_name"></el-table-column>
-          <el-table-column label="状态" prop="mg_state"></el-table-column>
-          <el-table-column label="操作"></el-table-column>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态" prop="mg_state">
+          <template slot-scope="scope">
+            <!-- 拿到这一行的数据 -->
+            <!-- {{scope.row}} -->
+            <!-- {{ scope.row }} -->
+            <el-switch v-model="scope.row.mg_state" @change="usesrStateChanged(scope.row)"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template >
+            <!-- 拿到这一行的数据 -->
+            <!-- {{scope.row}} -->
+            <!-- {{ scope.row }} -->
+            <!-- 修改按钮 -->
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <!-- 删除按钮 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <!-- 分配角色按钮 -->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable='false'>
+                   <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+           </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
+      <!-- 分页区域 -->
+      <!-- layout字符串，展示功能 -->
+       <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="queryInfo.pagenum"
+      :page-sizes="[1, 2, 5, 10]"
+      :page-size="queryInfo.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            //获取用户列表的参数对象
-            queryInfo: {
-                query: '',
-                pagenum: 1,
-                pagesize: 2
-            },
-            userlist: [],
-            total: 0
-        }
+  data() {
+    return {
+      //获取用户列表的参数对象
+      queryInfo: {
+        query: "",
+        // 当前的页数
+        pagenum: 1,
+        //当前每页显示多少条数据
+        pagesize: 2,
+      },
+      userlist: [],
+      total: 0,
+    };
+  },
+  created() {
+    this.getUserList();
+  },
+  methods: {
+    //get请求参数拼接用params
+    async getUserList() {
+      const { data: res } = await this.$http.get("users", {
+        params: this.queryInfo,
+      });
+      if (res.meta.status !== 200)
+        return this.$message.error("获取用户列表失败！");
+      this.userlist = res.data.users;
+      this.total = res.data.total;
+      console.log(res);
     },
-    created(){
-        this.getUserList() 
-    },
-    methods: {
-        //get请求参数拼接用params
-        async getUserList() {
-            const {data: res} = await this.$http.get('users',{params: this.queryInfo})
-            if(res.meta.status !== 200) return this.$message.error('获取用户列表失败！')
-            this.userlist = res.data.users
-            this.total = res.data.total
-            console.log(res)
-        }
-    }
+    //监听pagesize改变的事件
+  handleSizeChange(newSize) {
+    console.log(newSize)
+    this.queryInfo.pagesize = newSize
+    this.getUserList()
+  },
+  //监听 页码值，改变的事件
+  handleCurrentChange(newPage){
+    console.log(newPage)
+    this.queryInfo.pagenum = newPage
+    // 重新获取数据
+    this.getUserList()
+
+  },
+  // 监听switch开关状态的改变
+  async usesrStateChanged(userinfo) {
+        console.log(userinfo)
+        const {data:res} = await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
+  if (res.meta.status !== 200) {
+    userinfo.mg_state = !userinfo.mg_state
+    return this.$message.error('更新用户状态失败')
+  }
+  console.log(res)
+  this.$message.success('更新用户状态成功')
+  }
+  },
 };
 </script>
 
