b1fa3b24661709b5048cfbbef6e015c3d7e1ec10
WhattheFx <2983313676@qq.com>
2021-03-07 21:29:17 +0800

    初步完善了商品分类列表

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -8,67 +8,147 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 ，el-row是行，el-row是列-->
     <el-card>
-        <el-row>
-            <el-rol>
-                <el-button type="primary">
-                    添加分类
-                </el-button>
-            </el-rol>
-        </el-row>
-        
-        <!-- 表格 -->
-        <tree-table :data='catelist' :columns='columns' :selection-type="false" :expand-type="false" :show-index="true" index-text="#" border :show-row-hover="false">
-        </tree-table>
-
-        <!-- 分页区域 -->
-
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="showAddCateDialog"> 添加分类 </el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+      <tree-table
+        class="treeTable"
+        :data="catelist"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        :show-index="true"
+        index-text="#"
+        border
+        :show-row-hover="false"
+      >
+      <!-- 是否有效模板 -->
+      <template slot="isok" slot-scope="scope">
+          <i class="el-icon-success" v-if='scope.row.cat_deleted ===false' style="color:lightgreen"></i>
+          <i class="el-icon-error" v-else style="color:red"></i>
+      </template>
+       <!-- 排序模板 -->
+      <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
+          <el-tag size="mini" type="success" v-else-if="scope.row.cat_level ===1">二级</el-tag>
+          <el-tag size="mini" type="warning" v-else>三级</el-tag>
+      </template>
+     <!-- 排序模板 -->
+      <template slot="opt" slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+      </template>
+      </tree-table>
+      <!-- 分页区域 ，page-sizes每页显示几条，page-size当前页显示几条，layout当前的分页区域可以由哪些部分组成，直接默认，total是总数据条数-->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="queryInfo.pagenum"
+      :page-sizes="[3, 5, 10, 15]"
+      :page-size="queryInfo.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
     </el-card>
+    <!-- 添加分类的对话框 -->
+<el-dialog
+  title="添加分类"
+  :visible.sync="addCatedialogVisible "
+  width="50%">
+  <span>这是一段信息</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="addCatedialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addCatedialogVisible  = false">确 定</el-button>
+  </span>
+</el-dialog>
   </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            // 参数,查询条件
-            queryInfo: {
-                type: 3,
-                // 第一页
-                pagenum: 1,
-                // 每页显示多少条
-                pagesize: 5
-            },
-            total: 0,
-            // 为table指定列的定义
-            columns: [
-                {label:'分类名称',prop:'cat_name'},
-            ],
-            // 商品分类的数据列表，默认为空
-            catelist: []
+  data() {
+    return {
+      // 参数,查询条件
+      queryInfo: {
+        type: 3,
+        // 第一页
+        pagenum: 1,
+        // 每页显示多少条
+        pagesize: 5,
+      },
+      total: 0,
+      // 为table指定列的定义
+      columns: [
+        { label: "分类名称", prop: "cat_name" },
+        {
+          label: "是否有效",
+          //表示，当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用的模板名称
+          template: "isok",
+        },
+        {
+          label: "排序",
+          //表示，当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用的模板名称
+          template: "order",
+        },
+        {
+          label: "操作",
+          //表示，当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用的模板名称
+          template: "opt",
         }
+      ],
+      // 商品分类的数据列表，默认为空
+      catelist: [],
+      //控制添加分类的对话框的分类与隐藏
+    addCatedialogVisible:false
+    };
+  },
+  created() {
+    this.getCateList();
+  },
+  methods: {
+    // 获取商品分类数据
+    async getCateList() {
+      const { data: res } = await this.$http.get("categories", {
+        params: this.queryInfo,
+      });
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取商品列表数据失败");
+      }
+      //   console.log(res.data);
+      this.$message.success("获取商品列表数据成功");
+      // 把数据列表，赋值给catelist
+      this.catelist = res.data.result;
+      //为总数据条数
+      this.total = res.data.total;
     },
-    created() {
+    //监听pagesize的改变,触发了这个函数即可以拿到最新的pagesize
+    handleSizeChange(newSize) {
+        this.queryInfo.pagesize = newSize
+        //重新刷新数据
         this.getCateList()
     },
-    methods: {
-        // 获取商品分类数据
-       async getCateList() {
-          const {data: res} = await this.$http.get('categories',{params: this.queryInfo})
-
-          if(res.meta.status !== 200) {
-              return this.$message.error('获取商品列表数据失败');
-
-          }
-        //   console.log(res.data);
-        this.$message.success('获取商品列表数据成功');
-        // 把数据列表，赋值给catelist
-        this.catelist = res.data.result
-        //为总数据条数
-        this.total = res.data.total
-
-        }
+    handleCurrentChange(newPage) {
+        this.queryInfo.pagenum = newPage
+        //重新刷新数据
+        this.getCateList()
+    },
+    //点击按钮，展示添加分类的对话框
+    showAddCateDialog(){
+        this.addCatedialogVisible =true
     }
-
+  },
 };
 </script>
 <style lang="less" scoped>
+.treeTable{
+    margin: 15px;
+}
 </style>
--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -1,5 +1,166 @@
 <template>
     <div>
-        参数列表组件
+    <!-- <h3>面包屑导航区域</h3> -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+    <!-- 头部警告区域 -->
+    <el-alert
+    title="注意： 只允许为第三级分类设置相关参数"
+    type="warning"
+    show-icon
+    :closable="false"
+    >
+  </el-alert>
+   <!-- 选择商品分类区域 -->
+   <el-row class="cat_opt">
+       <el-col>
+           <span>选择商品分类：</span>
+   <!-- 选择商品分类的级联选择框 -->
+    <el-cascader
+    v-model="seletedCateKeys"
+    :options="catelist"
+    :props="cateProps"
+    @change="handleChange"></el-cascader>
+       </el-col>
+   </el-row>
+
+  <!-- tab 页签区域 -->
+  <el-tabs v-model="activeName" @tab-click="handleTabClick">
+      <!-- 添加动态参数的面板 -->
+    <el-tab-pane label="动态参数" name="many">
+        <!-- 添加参数的按钮 -->
+        <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加参数</el-button>
+        <!-- 动态参数表格 -->
+        <el-table :data="manyTableData" border stripe>
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作" >
+                <template slot-scope="scope">
+                    <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                    <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </el-tab-pane>
+    <!-- 添加静态属性的面板 -->
+    <el-tab-pane label="静态属性" name="only">
+        <!-- 添加属性的按钮 -->
+         <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加属性</el-button>
+         <!-- 静态属性表格 -->
+                <el-table :data="onlyTableData" border stripe>
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作" >
+                <template slot-scope="scope">
+                    <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                    <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </el-tab-pane>
+  </el-tabs>
+    </el-card>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            //商品分类列表
+            catelist:[],
+            // 级联选择框的配置对象
+            cateProps: {
+                value:'cat_id',
+                label:'cat_name',
+                children:'children'
+            },
+            //级联选择框双向绑定到的数组
+            seletedCateKeys: [],
+            //被激活的页签的名称，默认选择第一个页签first,如果默认为配置管理，
+            activeName:'many',
+            //动态参数的数据
+            manyTableData:[],
+            //静态属性的数据
+            onlyTableData:[]
+        }
+    },
+    created() {
+        this.getCateList()
+    },
+    methods: {
+        async getCateList() {
+            const {data: res} = await this.$http.get('categories')
+            if(res.meta.status !== 200) {
+                return this.$message.error('获取商品分类失败')
+            }
+         this.catelist = res.data
+         console.log(this.catelist)
+        },
+        //级联选择框选中项变化，会触发这个函数
+         handleChange() {
+             this.getParamsData()
+         },
+        // tab 页签点击事件的处理函数
+        handleTabClick() {
+            console.log(this.activeName)
+            this.getParamsData()
+        },
+        //获取参数的列表数据
+       async getParamsData() {
+        console.log(this.seletedCateKeys)
+        if(this.seletedCateKeys.length !== 3){
+            this.seletedCateKeys = []
+            return
+        }
+        //如果是三级标题
+        console.log(this.seletedCateKeys)
+        // 根据所选分类的Id,和当前所处的面板，获取对应的参数
+        // this.$http.get('categories/:id/attributes')
+       const {data:res} = await this.$http.get(`categories/${this.cateId}/attributes`,{params:{sel:this.activeName}})
+         
+         if(res.meta.status !==200 ) {
+             return this.$message.error('获取参数列表数据失败')
+         }
+         console.log(res.data)
+         if(this.activeName === 'many') {
+             this.manyTableData = res.data
+         }else {
+             this.onlyTableData = res.data
+         }
+        }
+    },
+    computed: {
+        //如果按钮需要被禁用，则返回true，否则返回false
+        isBtnDisabled() {
+            if (this.seletedCateKeys.length !== 3){
+                return true
+            }
+            return false
+        },
+        //当前选中的三级分类的Id
+        cateId() {
+            if(this.seletedCateKeys.length ===3){
+                return this.seletedCateKeys[2]
+            }
+            return null
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.cat_opt {
+    margin: 15px 0;
+}
+</style>
+© 2021 GitHub, Inc.
